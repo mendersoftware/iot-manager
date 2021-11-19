42821638beee13fe9e00518437b5b020e801e3cb
Alf-Rune Siqveland <alf.rune@northern.tech>
2021-11-19 17:11:46 +0100

    app: Unit tests for provisioning and deleteing devices
    changelog: none
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- app/app_test.go
+++ app/app_test.go
@@ -22,9 +22,14 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
+	"github.com/mendersoftware/azure-iot-manager/client/iothub"
+	miothub "github.com/mendersoftware/azure-iot-manager/client/iothub/mocks"
+	mworkflows "github.com/mendersoftware/azure-iot-manager/client/workflows/mocks"
 	"github.com/mendersoftware/azure-iot-manager/model"
 	storeMocks "github.com/mendersoftware/azure-iot-manager/store/mocks"
 )
+
+var contextMatcher = mock.MatchedBy(func(ctx context.Context) bool { return true })
 
 func TestHealthCheck(t *testing.T) {
 	testCases := []struct {
@@ -156,6 +161,322 @@ func TestSetSettings(t *testing.T) {
 			err := app.SetSettings(ctx, tc.SetSettingsSettings)
 			if tc.SetSettingsError != nil {
 				assert.EqualError(t, err, tc.SetSettingsError.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestProvisionDevice(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		Name string
+
+		ConnStr  *model.ConnectionString
+		DeviceID string
+
+		Store func(t *testing.T, self *testCase) *storeMocks.DataStore
+		Hub   func(t *testing.T, self *testCase) *miothub.Client
+		Wf    func(t *testing.T, self *testCase) *mworkflows.Client
+
+		Error error
+	}
+	testCases := []testCase{{
+		Name: "ok",
+
+		ConnStr: &model.ConnectionString{
+			HostName: "localhost",
+			Key:      []byte("super secret"),
+			Name:     "my favorite string",
+		},
+		DeviceID: "68ac6f41-c2e7-429f-a4bd-852fac9a5045",
+
+		Store: func(t *testing.T, self *testCase) *storeMocks.DataStore {
+			store := new(storeMocks.DataStore)
+			store.On("GetSettings", contextMatcher).
+				Return(model.Settings{
+					ConnectionString: self.ConnStr,
+				}, nil)
+			return store
+		},
+		Hub: func(t *testing.T, self *testCase) *miothub.Client {
+			hub := new(miothub.Client)
+			hub.On("UpsertDevice", contextMatcher, self.ConnStr, self.DeviceID).
+				Return(&iothub.Device{
+					DeviceID: self.DeviceID,
+					Auth: &iothub.Auth{
+						Type: iothub.AuthTypeSymmetric,
+						SymmetricKey: &iothub.SymmetricKey{
+							Primary:   iothub.Key("key1"),
+							Secondary: iothub.Key("key2"),
+						},
+					},
+				}, nil)
+			return hub
+		},
+		Wf: func(t *testing.T, self *testCase) *mworkflows.Client {
+			wf := new(mworkflows.Client)
+			primKey := &model.ConnectionString{
+				Key:      []byte("key1"),
+				DeviceID: self.DeviceID,
+				HostName: self.ConnStr.HostName,
+			}
+			secKey := &model.ConnectionString{
+				Key:      []byte("key2"),
+				DeviceID: self.DeviceID,
+				HostName: self.ConnStr.HostName,
+			}
+			wf.On("SubmitDeviceConfiguration",
+				contextMatcher,
+				self.DeviceID,
+				map[string]string{
+					confKeyPrimaryKey:   primKey.String(),
+					confKeySecondaryKey: secKey.String(),
+				}).Return(nil)
+			return wf
+		},
+	}, {
+		Name: "error/device does not have a connection string",
+
+		ConnStr: &model.ConnectionString{
+			HostName: "localhost",
+			Key:      []byte("super secret"),
+			Name:     "my favorite string",
+		},
+		DeviceID: "68ac6f41-c2e7-429f-a4bd-852fac9a5045",
+
+		Store: func(t *testing.T, self *testCase) *storeMocks.DataStore {
+			store := new(storeMocks.DataStore)
+			store.On("GetSettings", contextMatcher).
+				Return(model.Settings{
+					ConnectionString: self.ConnStr,
+				}, nil)
+			return store
+		},
+		Hub: func(t *testing.T, self *testCase) *miothub.Client {
+			hub := new(miothub.Client)
+			hub.On("UpsertDevice", contextMatcher, self.ConnStr, self.DeviceID).
+				Return(&iothub.Device{
+					DeviceID: self.DeviceID,
+					Auth: &iothub.Auth{
+						Type: iothub.AuthTypeNone,
+					},
+				}, nil)
+			return hub
+		},
+		Wf: func(t *testing.T, self *testCase) *mworkflows.Client {
+			return new(mworkflows.Client)
+		},
+
+		Error: ErrNoDeviceConnectionString,
+	}, {
+		Name: "error/hub failure",
+
+		ConnStr: &model.ConnectionString{
+			HostName: "localhost",
+			Key:      []byte("super secret"),
+			Name:     "my favorite string",
+		},
+		DeviceID: "68ac6f41-c2e7-429f-a4bd-852fac9a5045",
+
+		Store: func(t *testing.T, self *testCase) *storeMocks.DataStore {
+			store := new(storeMocks.DataStore)
+			store.On("GetSettings", contextMatcher).
+				Return(model.Settings{
+					ConnectionString: self.ConnStr,
+				}, nil)
+			return store
+		},
+		Hub: func(t *testing.T, self *testCase) *miothub.Client {
+			hub := new(miothub.Client)
+			hub.On("UpsertDevice", contextMatcher, self.ConnStr, self.DeviceID).
+				Return(nil, errors.New("internal error"))
+			return hub
+		},
+		Wf: func(t *testing.T, self *testCase) *mworkflows.Client {
+			return new(mworkflows.Client)
+		},
+
+		Error: errors.New("failed to update iothub devices: internal error"),
+	}, {
+		Name: "error/no connection string",
+
+		DeviceID: "68ac6f41-c2e7-429f-a4bd-852fac9a5045",
+
+		Store: func(t *testing.T, self *testCase) *storeMocks.DataStore {
+			store := new(storeMocks.DataStore)
+			store.On("GetSettings", contextMatcher).
+				Return(model.Settings{}, nil)
+			return store
+		},
+		Hub: func(t *testing.T, self *testCase) *miothub.Client {
+			return new(miothub.Client)
+		},
+		Wf: func(t *testing.T, self *testCase) *mworkflows.Client {
+			return new(mworkflows.Client)
+		},
+
+		Error: ErrNoConnectionString,
+	}, {
+		Name: "error/getting settings",
+
+		DeviceID: "68ac6f41-c2e7-429f-a4bd-852fac9a5045",
+
+		Store: func(t *testing.T, self *testCase) *storeMocks.DataStore {
+			store := new(storeMocks.DataStore)
+			store.On("GetSettings", contextMatcher).
+				Return(model.Settings{}, errors.New("wut?"))
+			return store
+		},
+		Hub: func(t *testing.T, self *testCase) *miothub.Client {
+			return new(miothub.Client)
+		},
+		Wf: func(t *testing.T, self *testCase) *mworkflows.Client {
+			return new(mworkflows.Client)
+		},
+
+		Error: errors.New("failed to retrieve settings: wut?"),
+	}}
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			ds := tc.Store(t, &tc)
+			hub := tc.Hub(t, &tc)
+			wf := tc.Wf(t, &tc)
+			defer ds.AssertExpectations(t)
+			defer hub.AssertExpectations(t)
+			defer wf.AssertExpectations(t)
+
+			app := New(ds, hub, wf)
+			err := app.ProvisionDevice(ctx, tc.DeviceID)
+
+			if tc.Error != nil {
+				if assert.Error(t, err) {
+					assert.Regexp(t, tc.Error.Error(), err.Error())
+				}
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestDecommissionDevice(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		Name string
+
+		ConnStr  *model.ConnectionString
+		DeviceID string
+
+		Store func(t *testing.T, self *testCase) *storeMocks.DataStore
+		Hub   func(t *testing.T, self *testCase) *miothub.Client
+
+		Error error
+	}
+	testCases := []testCase{{
+		Name: "ok",
+
+		ConnStr: &model.ConnectionString{
+			HostName: "localhost",
+			Key:      []byte("super secret"),
+			Name:     "my favorite string",
+		},
+		DeviceID: "68ac6f41-c2e7-429f-a4bd-852fac9a5045",
+
+		Store: func(t *testing.T, self *testCase) *storeMocks.DataStore {
+			store := new(storeMocks.DataStore)
+			store.On("GetSettings", contextMatcher).
+				Return(model.Settings{
+					ConnectionString: self.ConnStr,
+				}, nil)
+			return store
+		},
+		Hub: func(t *testing.T, self *testCase) *miothub.Client {
+			hub := new(miothub.Client)
+			hub.On("DeleteDevice", contextMatcher, self.ConnStr, self.DeviceID).
+				Return(nil)
+			return hub
+		},
+	}, {
+		Name: "error/hub failure",
+
+		ConnStr: &model.ConnectionString{
+			HostName: "localhost",
+			Key:      []byte("super secret"),
+			Name:     "my favorite string",
+		},
+		DeviceID: "68ac6f41-c2e7-429f-a4bd-852fac9a5045",
+
+		Store: func(t *testing.T, self *testCase) *storeMocks.DataStore {
+			store := new(storeMocks.DataStore)
+			store.On("GetSettings", contextMatcher).
+				Return(model.Settings{
+					ConnectionString: self.ConnStr,
+				}, nil)
+			return store
+		},
+		Hub: func(t *testing.T, self *testCase) *miothub.Client {
+			hub := new(miothub.Client)
+			hub.On("DeleteDevice", contextMatcher, self.ConnStr, self.DeviceID).
+				Return(errors.New("internal error"))
+			return hub
+		},
+
+		Error: errors.New("failed to delete IoT Hub device: internal error"),
+	}, {
+		Name: "error/no connection string",
+
+		DeviceID: "68ac6f41-c2e7-429f-a4bd-852fac9a5045",
+
+		Store: func(t *testing.T, self *testCase) *storeMocks.DataStore {
+			store := new(storeMocks.DataStore)
+			store.On("GetSettings", contextMatcher).
+				Return(model.Settings{}, nil)
+			return store
+		},
+		Hub: func(t *testing.T, self *testCase) *miothub.Client {
+			return new(miothub.Client)
+		},
+
+		Error: ErrNoConnectionString,
+	}, {
+		Name: "error/getting settings",
+
+		DeviceID: "68ac6f41-c2e7-429f-a4bd-852fac9a5045",
+
+		Store: func(t *testing.T, self *testCase) *storeMocks.DataStore {
+			store := new(storeMocks.DataStore)
+			store.On("GetSettings", contextMatcher).
+				Return(model.Settings{}, errors.New("wut?"))
+			return store
+		},
+		Hub: func(t *testing.T, self *testCase) *miothub.Client {
+			return new(miothub.Client)
+		},
+
+		Error: errors.New("failed to retrieve settings: wut?"),
+	}}
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			ds := tc.Store(t, &tc)
+			hub := tc.Hub(t, &tc)
+			defer ds.AssertExpectations(t)
+			defer hub.AssertExpectations(t)
+
+			app := New(ds, hub, nil)
+			err := app.DeleteIOTHubDevice(ctx, tc.DeviceID)
+
+			if tc.Error != nil {
+				if assert.Error(t, err) {
+					assert.Regexp(t, tc.Error.Error(), err.Error())
+				}
 			} else {
 				assert.NoError(t, err)
 			}
