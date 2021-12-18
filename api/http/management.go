75d1cc5a75298ae5d80c0f0df797751774ac6dd4
Alf-Rune Siqveland <alf.rune@northern.tech>
2021-12-18 14:07:27 +0100

    api: Rip out the Azure HTTP proxy
    changelog: none
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- api/http/management.go
+++ api/http/management.go
@@ -15,14 +15,8 @@
 package http
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"io"
-	"net"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
@@ -40,8 +34,6 @@ const (
 	AzureURIDeviceTwin    AzureDeviceURI = "/twins/:id"
 	AzureURIDevice        AzureDeviceURI = "/devices/:id"
 	AzureURIDeviceModules AzureDeviceURI = "/devices/:id/modules"
-
-	defaultTTL = time.Minute
 )
 
 type AzureDeviceURI string
@@ -51,30 +43,8 @@ func (s AzureDeviceURI) URI(deviceID string) string {
 }
 
 const (
-	HdrKeyAuthz       = "Authorization"
-	HdrKeyXFF         = "X-Forwarded-For"
-	HdrKeyMSRequestID = "X-Ms-Request-Id"
+	HdrKeyAuthz = "Authorization"
 )
-
-// Hop-by-hop headers (RFC2616 section 13.5.1)
-// http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
-var hbhHeaders = [...]string{
-	"Connection",
-	"Keep-Alive",
-	"Proxy-Authenticate",
-	"Proxy-Authorization",
-	"Te",
-	"Trailers",
-	"Transfer-Encoding",
-	"Upgrade",
-	"X-Men-Requestid",
-}
-
-func delHbHHeaders(header http.Header) {
-	for _, hdr := range hbhHeaders {
-		delete(header, hdr)
-	}
-}
 
 var (
 	ErrMissingUserAuthentication = errors.New(
@@ -85,121 +55,6 @@ var (
 
 // ManagementHandler is the namespace for management API handlers.
 type ManagementHandler APIHandler
-
-func (h *ManagementHandler) proxyAzureRequest(c *gin.Context, dstPath string) {
-	req := c.Request
-	ctx := req.Context()
-	settings, err := h.app.GetSettings(ctx)
-	switch {
-	case err != nil:
-		_ = c.Error(err)
-		rest.RenderError(c,
-			http.StatusInternalServerError,
-			errors.New(http.StatusText(http.StatusInternalServerError)),
-		)
-		return
-	case settings.ConnectionString == nil:
-		fallthrough
-	case settings.ConnectionString.Validate() != nil:
-		rest.RenderError(c, http.StatusConflict, ErrMissingConnectionString)
-		return
-	default:
-	}
-	cs := settings.ConnectionString
-	q := c.Request.URL.Query()
-	q.Set("api-version", AzureAPIVersion)
-	req.URL.Scheme = "https"
-	req.URL.RawQuery = q.Encode()
-	req.Host = cs.HostName
-	req.URL.Path = dstPath
-
-	req.URL.Host = cs.HostName
-	if cs.GatewayHostName != "" {
-		req.URL.Host = cs.GatewayHostName
-	}
-	req.RequestURI = ""
-
-	delHbHHeaders(req.Header)
-
-	var expireAt time.Time
-	if dl, ok := ctx.Deadline(); ok {
-		expireAt = dl
-	} else {
-		var cancel context.CancelFunc
-		expireAt = time.Now().Add(defaultTTL)
-		ctx, cancel = context.WithDeadline(ctx, expireAt)
-		defer cancel()
-		req = req.WithContext(ctx)
-	}
-	req.Header.Set(HdrKeyAuthz, cs.Authorization(expireAt))
-	if req.Header.Get(HdrKeyXFF) == "" {
-		if host, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
-			req.Header.Set(HdrKeyXFF, host)
-		}
-	}
-	rsp, err := h.Do(req)
-	if err != nil {
-		_ = c.Error(err)
-		rest.RenderError(c,
-			http.StatusBadGateway,
-			errors.New("failed to proxy request to IoT Hub"),
-		)
-		return
-	}
-	defer rsp.Body.Close()
-	delHbHHeaders(rsp.Header)
-	delete(rsp.Header, HdrKeyMSRequestID)
-	rspHdrs := c.Writer.Header()
-	for k, v := range rsp.Header {
-		rspHdrs[k] = v
-	}
-	c.Status(rsp.StatusCode)
-	_, err = io.Copy(c.Writer, rsp.Body)
-	if err != nil {
-		_ = c.Error(err)
-	}
-}
-
-func (h *ManagementHandler) GetDeviceModules(c *gin.Context) {
-	h.proxyAzureRequest(c, AzureURIDeviceModules.URI(c.Param("id")))
-}
-
-func (h *ManagementHandler) GetDevice(c *gin.Context) {
-	h.proxyAzureRequest(c, AzureURIDevice.URI(c.Param("id")))
-}
-
-// GET /device/:id/twin
-func (h *ManagementHandler) GetDeviceTwin(c *gin.Context) {
-	h.proxyAzureRequest(c, AzureURIDeviceTwin.URI(c.Param("id")))
-}
-
-// PATCH /device/:id/twin
-func (h *ManagementHandler) UpdateDeviceTwin(c *gin.Context) {
-	var schema struct {
-		Properties map[string]interface{} `json:"properties"`
-		Tags       map[string]interface{} `json:"tags,omitempty"`
-	}
-	var azureSchema struct {
-		Properties struct {
-			Desired map[string]interface{} `json:"desired"`
-		} `json:"properties"`
-		Tags map[string]interface{} `json:"tags,omitempty"`
-	}
-	err := c.ShouldBindJSON(&schema)
-	c.Request.Body.Close()
-	if err != nil {
-		rest.RenderError(c, http.StatusBadRequest, errors.Wrap(err,
-			"malformed request body",
-		))
-		return
-	}
-	azureSchema.Properties.Desired = schema.Properties
-	azureSchema.Tags = schema.Tags
-	b, _ := json.Marshal(azureSchema)
-	c.Request.Body = io.NopCloser(bytes.NewReader(b))
-	c.Request.ContentLength = int64(len(b))
-	h.proxyAzureRequest(c, AzureURIDeviceTwin.URI(c.Param("id")))
-}
 
 // GET /settings
 func (h *ManagementHandler) GetSettings(c *gin.Context) {
